6f9f2bb23397b091dea5655196bcf557743d806a
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-03-15 20:54:36 -0300

    function search

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
+extern crate minigrep_rust;
+
+use minigrep_rust::Config;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("argumentos insuficientes");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -27,12 +11,11 @@ fn main() {
         process::exit(1);
     });
 
-    let mut file = File::open(config.filename).expect("arquivo não encontrado");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents)
-        .expect("erro ao ler o arquivo");
-
-    println!("{}", contents);
+    if let Err(e) = minigrep_rust::run(config) {
+        println!("Erro na aplicação: {}", e);
+        process::exit(1);
+    };
 }
