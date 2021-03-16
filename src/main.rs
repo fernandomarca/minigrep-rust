34f95f1dfef1f4043f5fafc3aba28d5711c68350
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-03-15 21:58:27 -0300

    implement test and flow of errs

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problema ao passar os argumentos: {}", err);
+        eprintln!("Problema ao passar os argumentos: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep_rust::run(config) {
-        println!("Erro na aplicação: {}", e);
+        eprintln!("Erro na aplicação: {}", e);
         process::exit(1);
     };
 }
