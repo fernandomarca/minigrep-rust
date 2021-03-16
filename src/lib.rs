000bfb1dd3a771e9bf8dca144e7e064c858ee86d
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-03-16 13:41:01 -0300

    used iterators

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,16 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("argumentos insuficientes");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não foi possível pegar a palavra de busca"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não foi possível pegar o arquivo"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
@@ -48,13 +51,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line.trim());
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line.trim());
+    //     }
+    // }
+    // results
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
